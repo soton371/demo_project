d75dd79233762f9b999f64792a479f6cec654fd1
Soton Ahmed <soton371@gmail.com>
2024-09-12 06:55:49 +0600

    views and urls

--- home/views.py
+++ home/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render # type: ignore
+from django.http import HttpResponse # type: ignore
 
 # Create your views here.
+def home(request):
+    return HttpResponse("Start django server")
