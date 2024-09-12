3e9122e46e0a1528aec8d714b07aab8d117ad58c
Soton Ahmed <soton371@gmail.com>
2024-09-12 07:43:20 +0600

    model and migrations

--- home/models.py
+++ home/models.py
@@ -1,3 +1,14 @@
-from django.db import models
+from django.db import models # type: ignore
 
 # Create your models here.
+class Student(models.Model):
+    # id = models.AutoField()
+    name = models.CharField(max_length=100)
+    age = models.IntegerField()
+    email = models.EmailField()
+    address = models.TextField()
+    image = models.ImageField()
+    file = models.FileField()
+
+class Product(models.Model):
+    pass
